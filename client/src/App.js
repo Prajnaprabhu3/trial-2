26bc83111ccd1fd3e18b6a7043cb568980e4642a
Prajna <prajnapprabhu3@gmail.com>
2022-07-09 17:11:15 +0530

    init

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+
+            <p>This is a simple example of a React component.</p>
+        </div>
+    )
+}
+
+export default App;
---
+++ server/controllers/posts.js
@@ -0,0 +1,8 @@
+
+export const getPosts = (request, result) => {
+    result.send('THIS WORKS');
+}
+
+export const createPost = (request, result) => {
+    result.send('Post Creation');
+}
